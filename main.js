9cd10784038509bdd7c4ef56813f9f52746bb89a
Julie <juliy0214561@gmail.com>
2024-09-03 21:56:18 +0800

    [update] 上傳header

--- main.js
+++ main.js
@@ -1,2 +1,23 @@
 import "./assets/scss/all.scss";
 import "bootstrap/dist/js/bootstrap.min.js";
+import "https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js";
+
+/**
+ * 首頁header swiper
+ */
+var headerSwiper = new Swiper(".headerSwiper", {
+      centeredSlides: true,
+      slidesPerView: "auto",
+      coverflowEffect: {
+        rotate: 0,
+        modifier: 1,
+        slideShadows: true,
+      },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    slidesPerView: 1,
+    spaceBetween: 40,
+    centeredSlides: true,
+  });
